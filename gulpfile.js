685709667e14c7283ef7537557fe5ff44b42a370
Alexandr <net_box@ukr.net>
2021-10-03 22:47:46 +0300

    feat: add slider

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss         = require('gulp-sass');
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin     = require('gulp-imagemin');
+const del          = require('del');
+
+// browsersync - liveserver
+
+function browsersync(){
+    browserSync.init({
+        server : {
+            baseDir: 'app/'
+        }
+    });
+}
+
+// del - cleanDist
+
+function cleanDist(){
+    return del('dist')
+}
+
+// imagemin - image compression
+
+function images(){
+    return src('app/images/**/*')
+    .pipe(imagemin(
+        [
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]
+    ))
+    .pipe(dest('dist/images'))
+}
+
+// Library
+
+function scripts(){
+    return src([
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+// scss - css
+
+function styles(){
+    return src('app/scss/style.scss')
+        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist:['last 10 version'],
+            grid: true
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+// converts the finished project to a folder dist
+
+function build(){
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+        'app/*.html'
+    ], {base: 'app'})
+        .pipe(dest('dist'))
+}
+
+// tracks changes in the project
+
+function watching(){
+    watch(['app/scss/**/*.scss'], styles);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch(['app/*.html']).on('change', browserSync.reload);
+}
+
+//register
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(styles, scripts, browsersync, watching);
+
+
+
